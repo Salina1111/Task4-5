b996037ded2f36a7d698ba669294fb574266c455
Salina <salina.sallu@outlook.com>
2018-10-30 10:35:11 +0545

    Palindrome added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ function reverseString(str) {
     a[j] = str[i];
     return a.join('');
 }
+//Palindriome
+function reverseString(str) {
+    var a = [];
+    for (var i = str.length - 1, j = 0; i >= 0; i--, j++)
+    a[j] = str[i];
+    b = a.join('');
+     
+    if(b===str ){
+       alert(str+" " +"is palindrome");
+
+    }else{
+      alert(str+" "+"is not palindrome");
+    }
+return b;
+}
