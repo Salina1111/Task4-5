85d3480b6d7db1dd4a273b6b2705f4b753237aa3
Salina <salina.sallu@outlook.com>
2018-10-30 12:02:51 +0545

    fizzBuzz is added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-                       
+
 //Reverse string
 function reverseString(str) {
     var a = [];
@@ -12,14 +12,14 @@ function reverseString(str) {
     for (var i = str.length - 1, j = 0; i >= 0; i--, j++)
     a[j] = str[i];
     b = a.join('');
-     
+    
     if(b===str ){
-       alert(str+" " +"is palindrome");
-
+        alert(str+" " +"is palindrome");
+        
     }else{
-      alert(str+" "+"is not palindrome");
+        alert(str+" "+"is not palindrome");
     }
-return b;
+    return b;
 }
 reverseString("salina");
 
@@ -43,13 +43,13 @@ alert(reverse(12345));
 //Capitalize first letter
 function uppercase(str)
 {
-  var a1 = str.split(' ');
-  var a2 = [];
+    var a1 = str.split(' ');
+    var a2 = [];
     
-  for(var x = 0; x < a1.length; x++){
-      a2.push(a1[x].charAt(0).toUpperCase()+a1[x].slice(1));
-  }
-  return a2.join(' ');
+    for(var x = 0; x < a1.length; x++){
+        a2.push(a1[x].charAt(0).toUpperCase()+a1[x].slice(1));
+    }
+    return a2.join(' ');
 }
 console.log(uppercase("say hello"));
 alert(uppercase('say hello'));
@@ -57,7 +57,7 @@ alert(uppercase('say hello'));
 //Chunk Array
 function chunkArray(myArray, chunk_size){
     var results = [];
-    
+    console.log()
     while (myArray.length) {
         results.push(myArray.splice(0, chunk_size));
     }
@@ -69,3 +69,15 @@ function chunkArray(myArray, chunk_size){
 var result = chunkArray([1,2,3,4,5,6,7,8], 2);
 
 console.log(result);
+
+//FizzBuzz
+for (var i=1; i <= 100; i++){
+    if (i % 3 == 0){
+         console.log("Fizz");
+        }
+     else if(i % 5 == 0) {
+          console.log("Buzz");
+        }
+     else
+      console.log(i);
+ }
