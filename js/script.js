8559f736e49d1c9fc7f8bf22e2ffd24e2c291f85
Salina <salina.sallu@outlook.com>
2018-10-31 11:29:08 +0545

    combination is done

--- js/script.js
+++ js/script.js
@@ -120,3 +120,30 @@ function array(s){
 
 console.log(array(12345));
 
+
+
+//Combine number of given array into an arrat containing all combinations
+
+const powerSet = (array) => {
+    let power = [];
+    let total = Math.pow(2,array.length);
+    for(let i=0; i<total; i++)
+    {
+        let temp = [];
+        let num = i.toString(2);
+        while(num.length < array.length)
+        {
+            num = '0' + num;       
+        }
+        for (var b = 0; b < num.length; b++) 
+        {
+            if (num[b] === '1') 
+            { 
+                temp.push(array[b]);             }    
+        }
+        power.push(temp);
+    }
+    return power;
+}
+
+console.log(powerSet([1,2]));
